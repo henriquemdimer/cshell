f2520025bc9935383eca9a81bee3b20b0a64da95
Henrique M. Dimer <hdimer@tutamail.com>
2025-03-11 12:59:17 -0300

    feat: implement basic shell functionality
    This commit adds a basic shell implementation to the main.c file. The
    shell reads input, parses it into arguments, and handles built-in
    commands like `exit` and `cd`. It also supports executing external
    commands through forking and executing via `execvp()`.
    
    - Added function to remove whitespace from input tokens.
    - Implemented a loop to continuously read and process input commands.
    - Supported built-in `exit` and `cd` commands.
    - Added support for executing external commands and handling errors if
    the command is not found.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main() {
-	printf("Hello World!\n");
-	return 0;
+char *remove_whitespace(char *str)
+{
+    int i, j = 0;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ' && str[i] != '\n')
+        {
+            str[j++] = str[i];
+        }
+    }
+    str[j] = '\0';
+    return str;
+}
+
+int main()
+{
+    while (1)
+    {
+        char input[100];
+        printf("$ ");
+        fgets(input, sizeof(input), stdin);
+
+        char *buf = input;
+        char *token;
+
+        char *args[999];
+        int i = 0;
+        while ((token = strtok(buf, " ")) != NULL)
+        {
+            if (buf != NULL)
+                buf = NULL;
+
+            args[i++] = remove_whitespace(token);
+        }
+
+        int pid;
+        if (strcmp(args[0], "exit") == 0)
+        {
+            exit(0);
+        }
+        else if (strcmp(args[0], "cd") == 0)
+        {
+            char *path = args[1];
+			if (path == NULL) chdir(getenv("HOME"));
+			else chdir(path);
+        }
+        else
+        {
+            if ((pid = fork()) == 0)
+            {
+                if (execvp(args[0], args) != 0)
+                {
+                    printf("%s not found\n", args[0]);
+                    exit(1);
+                }
+                else
+                {
+                    exit(0);
+                }
+            }
+            else
+            {
+                waitpid(pid, 0, 0);
+            }
+        }
+
+		memset(args, 0, sizeof(args));
+    }
+
+    return 0;
 }
